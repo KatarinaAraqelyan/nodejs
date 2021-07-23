99104edf77a11996b064169167c5607b3c9d85c9
KatarinaAraqelyan <araqelyankatarina@gmail.com>
2021-07-23 11:26:47 +0400

    hello

--- Homework_2/homework4.js
+++
@@ -1,9 +0,0 @@
-const os = require('os')
-const fs = require('fs')
-const path = require('path')
-const currentDir = path.resolve()
-const file_path = path.join(currentDir,"write.txt")
- fs.readFile('input.txt',"utf8",function (error,text){
-   let result = text.replace(/ /g,"_")
-     fs.promises.writeFile(file_path,result)
-})
